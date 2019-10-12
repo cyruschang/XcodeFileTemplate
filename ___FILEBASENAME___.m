710b8f4610204dfc4c6eb506f05bdfab044cc418
常露阳 <changluyang@hualala.com>
2019-10-12 10:11:54 +0800

    控制器模板页面
    主要是对代码进行分区

---
+++ ___FILEBASENAME___.m
@@ -0,0 +1,58 @@
+//___FILEHEADER___
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - Lazy Load Data
+
+#pragma mark - Lazy Load UI
+
+#pragma mark - Life Circle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self baseInit];
+    [self initUI];
+}
+
+- (void)baseInit {
+    self.navigationItem.title = @"";
+}
+
+
+#pragma mark - UI
+
+- (void)initUI {
+    
+}
+
+#pragma mark - Functional Methods
+
+
+
+#pragma mark - API
+
+#pragma mark - TableView Delegate DataSource
+
+#pragma mark - Delegate
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
